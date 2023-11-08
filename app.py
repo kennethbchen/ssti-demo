5aa8a57e5a58c6ce0dea7c7a8b24e42963279226
kennethbchen <kennethbchen@gmail.com>
2023-11-08 17:44:55 -0500

    Implemented fixed version

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ app = Flask(__name__)
 def home():
     name = "World"
 
-
     if request.args.get("name"):
         name = request.args.get("name")
 
@@ -28,9 +27,23 @@ def home():
             <p>Shopping List:</p>
 
             <ul>
-                {% for item in ["Bread", "Apples", "Peaches", "Sugar", "Beans"] -%}
-                    <li>{{item}}</li>
-                {% endfor %}
+                    {%- for item in [
+                        {"name": "Bread", "purchased": True},
+                        {"name": "Bananas", "purchased": True},
+                        {"name": "Apples", "purchased": True},
+                        {"name": "Sugar", "purchased": False},
+                        {"name": "Beans", "purchased": False},
+                        {"name": "Carrots", "purchased": False}
+                        ]  
+                    -%}
+                    
+                        {% if item.purchased %}
+                            <li><s>{{item.name}}</s></li>
+                        {% else %}
+                            <li>{{item.name}}</li>
+                        {% endif %}
+                        
+                    {% endfor %}
             </ul>
         </body>
     </html>
@@ -38,6 +51,49 @@ def home():
 
     return render_template_string(template)
 
+@app.route("/fixed")
+def home_fixed():
+    name = "World"
+
+    if request.args.get("name"):
+        name = request.args.get("name")
+
+    shopping_list = [
+        {"name": "Bread", "purchased": True},
+        {"name": "Bananas", "purchased": False},
+        {"name": "Apples", "purchased": True},
+        {"name": "Sugar", "purchased": False},
+        {"name": "Beans", "purchased": False},
+        {"name": "Carrots", "purchased": False}
+    ]
+
+    template = """
+        <!DOCTYPE html>
+        <html>
+            <body>
+                <p>Hello {{name}}!</p>
+                <p>Shopping List:</p>
+                
+                <ul>
+                    {%- for item in shopping_list  -%}
+                    
+                        {% if item.purchased %}
+                            <li><s>{{item.name}}</s></li>
+                        {% else %}
+                            <li>{{item.name}}</li>
+                        {% endif %}
+                        
+                    {% endfor %}
+                </ul>
+                
+            </body>
+        </html>
+        """
+
+    return render_template_string(template, name=name, shopping_list=shopping_list)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
 
